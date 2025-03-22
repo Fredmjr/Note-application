f2481db5f56efe9051c097b2dcfc65a6f746a828
x2 <lunguj665@gmail.com>
2025-03-22 14:08:56 +0200

    first aapp

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from "express"
+
+const app = express()
+const port = 3000
+app.get("/", (req, res) => {
+  res.send("home ")
+})
+app.listen(port, () => {
+  console.log("hello your app is at 3000")
+})
